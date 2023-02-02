76cd656f841dd9b9128c71a93bfe11b39fc220b8
DutkaYaroslav <yaroslavdutka@gmail.com>
2023-02-02 18:02:59 +0200

    fixed problems with refresh

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
       <AuthContextProvider>
-          <BrowserRouter>
+          <BrowserRouter basename="/DutkaYaroslav">
               <App />
           </BrowserRouter>
       </AuthContextProvider>
